21fad9335c627fd75126e17b3d0d94e69effc771
darabperwaiz <darabperwaiz87@gmail.com>
2024-06-23 22:45:47 +0530

    deploye

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ import cors from "cors"
 const PORT = process.env.PORT || 4000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-app.use(cors())
 
 app.use('/api/v1/user/', userRouter)
 app.use('/api/v1/posts/', postRoute)
